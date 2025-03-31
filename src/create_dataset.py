3f73b50f1779db28582d70d530bb44b123df2e24
MinaAlmasi <mina.almasi00@gmail.com>
2025-03-31 12:14:33 +0200

    refactor: change model order

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -4,9 +4,12 @@ from pathlib import Path
 import polars as pl
 
 
+from utils.text_process import _remove_emojis_from_df
+
+
 DIR_NAMES = [
-            "google--gemma-3-12b-it",
             "meta-llama--Llama-3.1-8B-Instruct", 
+            "google--gemma-3-12b-it",
             "mistralai--Mistral-7B-Instruct-v0.3",
             "Qwen--Qwen2.5-7B-Instruct",
 ]
@@ -52,7 +55,10 @@ def read_data(data_dir: Path | list[Path]) -> pl.DataFrame:
 
     combined_df = pl.concat(dfs)
 
-    return combined_df
+    # clean for emojis
+    cleaned_combined_df = _remove_emojis_from_df(combined_df, "content")
+
+    return combined_df, cleaned_combined_df
 
 def main(): 
     version = 3.0
@@ -62,8 +68,13 @@ def main():
         for dir_name in DIR_NAMES
     ]
 
-    df = read_data(data_paths)
-    df.write_csv(Path(__file__).parents[1] / "data" / f"v{version}_dataset.csv")
+    combined_df, cleaned_combined_df = read_data(data_paths)
+    
+    # save raw
+    combined_df.write_csv(Path(__file__).parents[1] / "data" / f"v{version}_raw_dataset.csv")
+
+    # save cleaned
+    cleaned_combined_df.write_csv(Path(__file__).parents[1] / "data" / f"v{version}_dataset.csv")
 
 if __name__ == "__main__":
     main()
